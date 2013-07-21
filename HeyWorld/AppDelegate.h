0fa829900cb14367f99d88267c29a0a653fd5945
Dimitri MERAULT <d.merault@gmail.com>
2013-07-21 13:53:40 +0200

    Initial Commit

---
+++ HeyWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeyWorld
+//
+//  Created by Dimitri MERAULT on 21/07/13.
+//  Copyright (c) 2013 Dimitri MERAULT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
